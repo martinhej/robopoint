e9acf8131b37eac2ec243b2b647c979e6ca156ff
Martin Micuch <martinmicuch@Martins-MacBook-Pro.local>
2018-03-04 14:36:27 +0100

    Initial commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class IndexController extends Controller
+{
+    public function index() {
+        return $this->render('index.html.twig', array(
+            'rm' => 'robomaze',
+        ));
+    }
+
+}
