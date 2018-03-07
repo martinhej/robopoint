ff8d118fe4a9d9c19ab4d9aad577309f03f6543f
Martin Micuch <martin.micuch@gmail.com>
2018-03-07 11:10:28 +0100

    Add body template

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class IndexController extends Controller
 {
     public function index() {
-        return $this->render('index.html.twig', array(
+        return $this->render('base.html.twig', array(
             'rp' => 'robopoint',
         ));
     }
