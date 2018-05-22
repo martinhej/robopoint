3ae2064dfd17314b37b6b90b1df2ec9973387327
Martin Micuch <martin.micuch@gmail.com>
2018-05-22 10:30:32 +0200

    Provide endpoint to get single value

--- src/Controller/Api/V1/MessageController.php
+++ src/Controller/Api/V1/MessageController.php
@@ -7,11 +7,13 @@ use Psr\Log\LoggerInterface;
 use robocloud\Event\KinesisConsumerInMemoryErrorLogger;
 use robocloud\Exception\InvalidMessageClassException;
 use robocloud\Exception\InvalidMessageDataException;
+use robocloud\Exception\ShardInitiationException;
 use robocloud\Kinesis\Client\Consumer;
 use robocloud\Kinesis\Client\Producer;
 use robocloud\KinesisClientFactory;
 use robocloud\Message\Message;
 use robocloud\Message\MessageFactory;
+use robocloud\Message\MessageInterface;
 use robocloud\Message\MessageSchemaValidator;
 use robocloud\MessageProcessing\Backend\KeepInMemoryBackend;
 use robocloud\MessageProcessing\Filter\FilterByPurpose;
@@ -27,6 +29,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route ("/api/v1/messages")
@@ -58,6 +61,18 @@ class MessageController extends Controller
      * @var LoggerInterface
      */
     protected $logger;
+
+    /**
+     * Millis behind latest.
+     *
+     * @var int
+     */
+    protected $lag;
+
+    /**
+     * @var array
+     */
+    protected $errors = [];
 
     /**
      * MessageController constructor.
@@ -178,21 +193,64 @@ class MessageController extends Controller
     public function getActionReadByPurpose($roboId, $purpose)
     {
         $filter = new FilterByPurpose($purpose);
-        return $this->readMessages($roboId, $filter);
+        $messages = $this->readMessages($roboId, $filter);
+        return $this->getReadJsonResponse($messages);
     }
 
     /**
-     * @Route("/{roboId}/read-my")
+     * @Route("/{roboId}/read-from")
      * @Method("GET")
      *
      * @param string $roboId
      *
      * @return JsonResponse
      */
-    public function getActionReadMy($roboId)
+    public function getActionReadFrom($roboId)
     {
         $filter = new FilterByRoboId($roboId);
-        return $this->readMessages($roboId, $filter);
+        $messages = $this->readMessages($roboId, $filter);
+        return $this->getReadJsonResponse($messages);
+    }
+
+    /**
+     * @Route("/{roboId}/read-by-purpose/{purpose}/last/get-property/{property}")
+     * @Method("GET")
+     *
+     * @param string $roboId
+     * @param string $purpose
+     * @param string $property
+     *
+     * @return Response
+     */
+    public function getActionGetByPurposeLastByProperty($roboId, $purpose, $property) {
+        $filter = new FilterByPurpose($purpose);
+
+        do {
+            $messages = $this->readMessages($roboId, $filter);
+        }
+        while ($this->getLag() > 0);
+
+        $response = '';
+
+        if (!empty($messages)) {
+            $message = array_pop($messages);
+            $data = $message->getData();
+            if (!isset($data[$property])) {
+                return new Response('The provided property "' . $property . '" was not found.', 400);
+            }
+
+            $response = $data[$property];
+        }
+
+        if (!empty($this->errors['critical'])) {
+            return new Response('System error.', 500);
+        }
+        elseif (!empty($this->errors['user'])) {
+            return new Response(implode(' | ', $this->errors['user']), 400);
+        }
+
+        return new Response($response);
+
     }
 
     /**
@@ -201,7 +259,7 @@ class MessageController extends Controller
      * @param string $roboId
      * @param FilterInterface $filter
      *
-     * @return JsonResponse
+     * @return MessageInterface[]
      */
     protected function readMessages($roboId, FilterInterface $filter)
     {
@@ -225,31 +283,51 @@ class MessageController extends Controller
 
         try {
             $consumer->consume(0);
-        } catch (\Exception $e) {
-
-            $this->logger->error($e->getMessage(), ['exception' => $e]);
-
-            return new JsonResponse(['errors' => [
-                ['message' => 'System error.'],
-            ]], 500);
+        }
+        catch (ShardInitiationException $e) {
+            $this->logger->critical($e->getMessage(), ['exception' => $e]);
+            $this->errors['critical'][] = $e->getMessage();
         }
 
         if ($errors = $this->kinesisErrorHandler->getErrors()) {
 
             array_map(function ($error) {
-                $this->logger->error($error['message'], ['exception' => $error['exception']]);
+                if (is_a($error['exception'], InvalidMessageDataException::class)) {
+                    $this->logger->error($error['message'], ['exception' => $error['exception']]);
+                    $this->errors['user'][] = $error['message'];
+                }
+                else {
+                    $this->logger->critical($error['message'], ['exception' => $error['exception']]);
+                    $this->errors['critical'][] = $error['message'];
+                }
             }, $errors);
+        }
 
+        $this->lag = $consumer->getLag();
+
+        return $this->getBackend()->flush();
+
+    }
+
+    /**
+     * @param array $messages
+     * @return JsonResponse
+     */
+    protected function getReadJsonResponse(array $messages) {
+        if (!empty($this->errors['critical'])) {
             return new JsonResponse(['errors' => [
                 ['message' => 'System error.'],
             ]], 500);
         }
-
-        $messages = $this->getBackend()->flush();
+        elseif (!empty($this->errors['user'])) {
+            return new JsonResponse(['errors' => array_map(function($error) {
+                return ['message' => $error];
+            }, $this->errors['user'])], 400);
+        }
 
         return new JsonResponse([
             'messages' => $messages,
-            'lag' => $consumer->getLag(),
+            'lag' => $this->getLag(),
         ]);
     }
 
@@ -303,6 +381,15 @@ class MessageController extends Controller
     public function getKinesisErrorHandler(): KinesisConsumerInMemoryErrorLogger
     {
         return $this->kinesisErrorHandler;
+    }
+
+    /**
+     * Gets millis behind latest.
+     *
+     * @return int
+     */
+    public function getLag() {
+        return $this->lag;
     }
 
 }
